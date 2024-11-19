24fe3181fea7056a29bafecd46c69e3728ec78b4
~U.S.A~ <102748373+Stefanie-A@users.noreply.github.com>
2024-11-19 20:58:05 +0000

    fix: format

--- infrastructure/modules/variables.tf
+++ infrastructure/modules/variables.tf
@@ -1,17 +1,17 @@
 variable "pub_subnets_cidr" {
   description = "public subnet cidr block"
-  type = list(string)
-  default = [ "10.20.1.0/24", "10.20.2.0/24", "10.20.3.0/24" ]
+  type        = list(string)
+  default     = ["10.20.1.0/24", "10.20.2.0/24", "10.20.3.0/24"]
 }
 variable "pri_subnets_cidr" {
   description = "private subnet cidr block"
-  type = list(string)
-  default = [ "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24" ]
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 variable "azs" {
-  type = list(string)
+  type        = list(string)
   description = "Availablity zone for public subnet"
-  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 variable "key_name" {
   description = "Name of the SSH key pair"
@@ -19,27 +19,27 @@ variable "key_name" {
   default     = "my-key"
 }
 variable "region" {
-  type = string
+  type        = string
   description = "vpc region"
-  default = "us-east-1"
+  default     = "us-east-1"
 }
 variable "security_group" {
-  type = string
+  type        = string
   description = "security group"
-  default = "main_sg"
+  default     = "main_sg"
 }
 variable "instance_type" {
-  type = string
+  type        = string
   description = "Instance type for load balancer"
 }
 variable "ami" {
-  type = string
+  type        = string
   description = "Instance type for load balancer"
-  default = "ami-0866a3c8686eaeeba"    
+  default     = "ami-0866a3c8686eaeeba"
   validation {
-  condition     = length(var.ami) > 4 && substr(var.ami, 0, 4) == "ami-"
-  error_message = "Please provide a valid value for variable AMI."
- }
+    condition     = length(var.ami) > 4 && substr(var.ami, 0, 4) == "ami-"
+    error_message = "Please provide a valid value for variable AMI."
+  }
 }
 variable "db_username" {
   description = "RDS root username"
@@ -56,15 +56,15 @@ variable "db_password" {
 variable "allowed_repos_branches" {
   description = "GitHub repos/branches allowed to assume the IAM role."
   type = list(object({
-    org = string
-    repo = string
+    org    = string
+    repo   = string
     branch = string
   }))
-  default = [ 
+  default = [
     {
-    org = "Stefanie-A"
-    repo = "aws-3tier"
-    branch = "main"
- }
- ]
+      org    = "Stefanie-A"
+      repo   = "aws-3tier"
+      branch = "main"
+    }
+  ]
 }
