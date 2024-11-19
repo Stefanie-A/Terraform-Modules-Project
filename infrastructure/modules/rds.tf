0ed2424fb1d14f16bc0575f346680fd5f76de6d4
~U.S.A~ <102748373+Stefanie-A@users.noreply.github.com>
2024-11-19 20:53:27 +0000

    fix: format

--- infrastructure/modules/rds.tf
+++ infrastructure/modules/rds.tf
@@ -8,22 +8,22 @@ resource "aws_db_subnet_group" "db_subnet" {
 }
 
 resource "aws_db_instance" "app_db" {
-  identifier             = "3tier-database"
-  instance_class         = "db.t3.micro"
-  allocated_storage      = 5
-  engine                 = "postgres"
-  engine_version         = "14.1"
-  username               = var.db_username
-  password               = var.db_password
-  db_subnet_group_name   = aws_db_subnet_group.db_subnet.name
-  vpc_security_group_ids = [aws_security_group.sg.id]
-  parameter_group_name   = aws_db_parameter_group.db_para.name
-  publicly_accessible    = true
-  skip_final_snapshot    = false
+  identifier                = "3tier-database"
+  instance_class            = "db.t3.micro"
+  allocated_storage         = 5
+  engine                    = "postgres"
+  engine_version            = "14.1"
+  username                  = var.db_username
+  password                  = var.db_password
+  db_subnet_group_name      = aws_db_subnet_group.db_subnet.name
+  vpc_security_group_ids    = [aws_security_group.sg.id]
+  parameter_group_name      = aws_db_parameter_group.db_para.name
+  publicly_accessible       = true
+  skip_final_snapshot       = false
   final_snapshot_identifier = "db-snap"
-  backup_retention_period = 7
-  backup_window = "03:00-04:00"
-  maintenance_window = "mon:04:00-mon:04:30"
+  backup_retention_period   = 7
+  backup_window             = "03:00-04:00"
+  maintenance_window        = "mon:04:00-mon:04:30"
 }
 
 resource "aws_db_parameter_group" "db_para" {
