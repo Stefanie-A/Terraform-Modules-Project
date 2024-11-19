bc272161c5eb631d3aa276fcef6801783953b007
~U.S.A~ <102748373+Stefanie-A@users.noreply.github.com>
2024-11-19 20:59:18 +0000

    fix: format

--- infrastructure/modules/provider.tf
+++ infrastructure/modules/provider.tf
@@ -1,11 +1,15 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.0.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0.0"
     }
   }
   backend "s3" {
-    key = "state/terraform.tfstate"
-    }
+    key            = "state/terraform.tfstate"
+    bucket         = "terraformstate-file"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locks"
+    encrypt        = true
+  }
 }
