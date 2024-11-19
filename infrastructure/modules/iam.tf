0d037c75fe78b74020265994cd3b43f0051b0503
~U.S.A~ <102748373+Stefanie-A@users.noreply.github.com>
2024-11-19 20:56:34 +0000

    fix: format

--- infrastructure/modules/iam.tf
+++ infrastructure/modules/iam.tf
@@ -1,30 +1,30 @@
 # Create an IAM OIDC identity provider that trusts GitHub
 resource "aws_iam_openid_connect_provider" "github_actions" {
- url = "https://token.actions.githubusercontent.com"
- client_id_list = ["sts.amazonaws.com"]
- thumbprint_list = [
- data.tls_certificate.github.certificates[0].sha1_fingerprint
- ]
+  url            = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = [
+    data.tls_certificate.github.certificates[0].sha1_fingerprint
+  ]
 }
 # Fetch GitHub's OIDC thumbprint
 data "tls_certificate" "github" {
- url = "https://token.actions.githubusercontent.com"
+  url = "https://token.actions.githubusercontent.com"
 }
 
 data "aws_iam_policy_document" "assume_role_policy" {
-    statement {
-        actions = ["sts:AssumeRoleWithWebIdentity"]
-        effect = "Allow"
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
     principals {
-        identifiers = [aws_iam_openid_connect_provider.github_actions.arn]
-        type = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github_actions.arn]
+      type        = "Federated"
     }
     condition {
-        test = "StringEquals"
-        variable = "token.actions.githubusercontent.com:sub"
-        values = [
-            for a in var.allowed_repos_branches : "repo:${a["org"]}/${a["repo"]}:ref:refs/heads/${a["branch"]}"
-        ]
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:sub"
+      values = [
+        for a in var.allowed_repos_branches : "repo:${a["org"]}/${a["repo"]}:ref:refs/heads/${a["branch"]}"
+      ]
     }
-    }
+  }
 }
