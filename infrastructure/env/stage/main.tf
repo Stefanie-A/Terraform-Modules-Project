03f3446f6fd475d046bcca5fabeedc3353826feb
Stefanie-A <albertstephanie630@gmail.com>
2025-02-25 14:28:33 +0000

    chore: add variables

--- infrastructure/env/stage/main.tf
+++ infrastructure/env/stage/main.tf
@@ -4,13 +4,9 @@ provider "aws" {
 
 module "stage-app" {
   source = "../../modules/my-app"
-  region = "us-east-1"
-  key_name = "my-key"
-  instance_type = "t2.micro"
-  ami = "ami-0866a3c8686eaeeba"
-  db_username = "admin"     # RDS root username
-  db_password = "password"  # RDS root user password    
-  bucket_name = "my-bucket"   # S3 bucket for frontend            
-  domain_name = "example.com" # The domain name for the website.
-  state_bucket = "my-state-bucket" # S3 bucket name for state file
+  state_bucket = var.state_bucket
+  db_password = var.db_password
+  db_username = var.db_username
+  bucket_name = var.bucket_name
+  instance_type = var.instance_type
 }
