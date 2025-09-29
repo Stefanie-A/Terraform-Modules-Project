89ea1875e83773fd66336161ccc15adfe713741d
Stefanie-A <albertstephanie630@gmail.com>
2025-09-29 15:07:00 +0100

    fix module

--- infrastructure/env/stage/main.tf
+++ infrastructure/env/stage/main.tf
@@ -2,17 +2,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
-<<<<<<< HEAD
 module "staging" {
   source = "../../modules/root"
   state_bucket = var.state_bucket
-=======
-module "stage-app" {
-  source = "../../modules/my-app"
-  state_bucket = var.state_bucket
-  db_password = var.db_password
-  db_username = var.db_username
->>>>>>> b1b4f410bf2dfd9afe2f3c757fb09bf178e89bce
   bucket_name = var.bucket_name
   instance_type = var.instance_type
 }
--- infrastructure/modules/my-app/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  bucket_name   = "frontend-bucket4525"
-  instance_type = "t2.micro"
-}
